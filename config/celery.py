04e99c3d9b29df0f0bdd3c34d2445e33141705ce
hammed103 <hammedfree@gmail.com>
2022-12-22 15:55:56 +0100

    add profile models and signals

--- config/celery.py
+++ config/celery.py
@@ -5,7 +5,7 @@ from django.conf import settings
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
 
-app = Celery("config")
+app = Celery("config", broker="redis://redis:6379/0")
 
 app.config_from_object("django.conf:settings", namespace="CELERY")
 
